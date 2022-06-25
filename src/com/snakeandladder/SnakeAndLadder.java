b48b4c5fa9a340f11625b2951073613251212b54
JVignesh4 <vigneshbuji04@gmail.com>
2022-06-25 10:20:28 +0530

    Added: To Getting Ladder, Snake Or No-Play

--- src/com/snakeandladder/SnakeAndLadder.java
+++ src/com/snakeandladder/SnakeAndLadder.java
@@ -4,11 +4,28 @@ public class SnakeAndLadder {
     public static void main(String[] args) {
         int Player;
         int Position = 0;
+        final int LADDER = 2;
+        final int SNAKE = 1;
         int dieRolls = (int) Math.floor((Math.random() * 10) % 6 + 1);
-        System.out.println("Player position is 0");
-        System.out.println("Die is Rolling..... " + dieRolls);
-        int newPosition = Position + dieRolls;
-        System.out.println("Player Position " + newPosition);
+        int option = (int) Math.floor((Math.random() * 10) % 3);
+        System.out.println("Option " + option);
+        switch (option) {
+            case SNAKE:
+                System.out.println("Die Rolls ...... " + dieRolls);
+                Position = Position - dieRolls;
+                if (Position < 0){
+                    Position = 0;
+                }
+                System.out.println("You got bite by a Snake & Your Position Goes to " + Position);
+                break;
+            case LADDER:
+                System.out.println("Die Rolls ...... " + dieRolls);
+                Position = Position + dieRolls;
+                System.out.println("Got a Ladder & Your Position Goes to " + Position);
+                break;
+            default:
+                System.out.println("Player position is Same");
+        }
     }
 }
 
