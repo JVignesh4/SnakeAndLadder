fff13f5752992f2c4a31465d5a981c9c43e17c16
JVignesh4 <vigneshbuji04@gmail.com>
2022-06-25 21:17:45 +0530

    Added: Count Of Die Rolls to Win The Game

--- src/com/snakeandladder/SnakeAndLadder.java
+++ src/com/snakeandladder/SnakeAndLadder.java
@@ -2,27 +2,30 @@ package com.snakeandladder;
 
 public class SnakeAndLadder {
     public static void main(String[] args) {
-        int Player;
         int playerPosition = 0;
         final int LADDER = 2;
         final int SNAKE = 1;
+        final int NO_PLAY = 0;
+        int dieRolls = 0;
         while (playerPosition <= 100) {
-            int dieRolls = (int) Math.floor((Math.random() * 10) % 6 + 1);
+            dieRolls++;
+            int dieRolling = (int) (Math.floor(Math.random() * 10) % 6) + 1;
             int option = (int) Math.floor((Math.random() * 10) % 3);
 
             switch (option) {
+                case NO_PLAY:
+                    System.out.println("There will be no play. Player will be at same position");
+                    break;
                 case SNAKE:
-                    System.out.println("Die Rolls ...... " + dieRolls);
-                    System.out.println("Option is ...... " + option);
+                    System.out.println("Number on die is: " + dieRolling);
                     playerPosition = playerPosition - dieRolls;
                     System.out.println("You got a bite by Snake & Your Position Goes to " + playerPosition);
-                    if (playerPosition == 0) {
-                        System.out.println("Player position is Same " + playerPosition);
+                    if (playerPosition <= 0) {
+                        System.out.println("Player position is " + playerPosition);
                     }
                     break;
                 case LADDER:
-                    System.out.println("Die Rolls ...... " + dieRolls);
-                    System.out.println("Option is ...... " + option);
+                    System.out.println("Number on die is: " + dieRolling);
                     playerPosition = playerPosition + dieRolls;
                     if (playerPosition >= 100) {
                         playerPosition = 100;
@@ -30,10 +33,11 @@ public class SnakeAndLadder {
                     System.out.println("Got a Ladder & Your Position Goes to " + playerPosition);
                     break;
                 default:
-                    System.out.println("Please Try Roll Die Again");
+                    break;
             }
             playerPosition++;
         }
+        System.out.println("Total No Of DieRolls:" + dieRolls);
     }
 }
 
