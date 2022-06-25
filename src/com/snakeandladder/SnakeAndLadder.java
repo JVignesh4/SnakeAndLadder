1b6bc63c7809efc77843fb7aa024e397d3b342e3
JVignesh4 <vigneshbuji04@gmail.com>
2022-06-25 10:12:58 +0530

    Added: Die Rolls To Get Number 1 To 6

--- src/com/snakeandladder/SnakeAndLadder.java
+++ src/com/snakeandladder/SnakeAndLadder.java
@@ -4,8 +4,11 @@ public class SnakeAndLadder {
     public static void main(String[] args) {
         int Player;
         int Position = 0;
-        System.out.println("Let's start a game..... ");
+        int dieRolls = (int) Math.floor((Math.random() * 10) % 6 + 1);
         System.out.println("Player position is 0");
+        System.out.println("Die is Rolling..... " + dieRolls);
+        int newPosition = Position + dieRolls;
+        System.out.println("Player Position " + newPosition);
     }
 }
 
